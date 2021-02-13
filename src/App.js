7682f049be127362c1d6f3fb5a3924e5faa41d5c
Arman <armansidhu3@gmail.com>
2021-02-13 12:50:00 +0530

    Added social leaderboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Social from './Social';
+import Scoreboard from './Scoreboard'
 
 import Dashboard from './Dashboard.js';
 
