f70738db0b5867592047ad870fc03746acb6c6e9
Trevor <tr89on@gmail.com>
2021-02-12 20:34:34 -0800

    added temporary dashboard

--- src/App.js
+++ src/App.js
@@ -2,23 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Dashboard from './Dashboard.js';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <nav id="navbar" className="navbar fixed-top navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a href="#navbar" className="navbar-brand">
+            Test
+          </a>
+          <a href="sidebar" className="navbar-text">
+            Test
+          </a>
+        </div>
+      </nav>
+      <div className="wrapper">
+        <nav id="sidebar">
+          <div className="sidebar-header">
+              <h3>Bootstrap Sidebar</h3>
+          </div>
+          <ul className="list-unstyled components">
+            <li>
+              Test item
+            </li>
+            <li>
+              Test item
+            </li>
+            <li>
+              Test item
+            </li>
+            <li>
+              Test item
+            </li>
+          </ul>
+        </nav>
+
+        <div id="content">
+          
+        </div>
+      </div> */}
+
+      <Dashboard />
     </div>
   );
 }
