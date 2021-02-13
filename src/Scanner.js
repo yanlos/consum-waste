5e1b74446f8fcd5bbaa656b97653d0f47508fc31
ChingChang9 <chingtheprogrammer@gmail.com>
2021-02-13 13:23:31 -0500

    embed scanner in main framework

--- src/Scanner.js
+++ src/Scanner.js
@@ -8,11 +8,10 @@ function Scanner() {
 
   return (
     <div>
-      <button onClick={() => setScanning(!scanning) }>{scanning ? 'Stop' : 'Start'}</button>
-      <div ref={scannerRef} style={{position: 'relative', border: '3px solid red'}}>
+      <button onClick={() => setScanning(!scanning) } style={{marginTop: 100}}>{scanning ? 'Stop' : 'Start'}</button>
+      <div ref={scannerRef} style={{position: "relative"}}>
         <canvas className="drawingBuffer" style={{
-          position: 'absolute',
-          top: '0px',
+          position: "absolute",
           border: '3px solid green',
         }} width="640" height="480" />
         {scanning ? <Camera scannerRef={scannerRef} onDetected={result => setScanning(false)} /> : null}
