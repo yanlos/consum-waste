a2d497e2f636e8a8897904bda675a8d1f1f56e5a
ChingChang9 <chingtheprogrammer@gmail.com>
2021-02-13 19:43:36 -0500

    redirect to footprint after scane (with info of the item)

--- src/Scanner.js
+++ src/Scanner.js
@@ -3,20 +3,25 @@ import './App.css';
 import Camera from './Camera';
 import './css/style.css'
 
-function Scanner() {
+function Scanner({ setActivePage, setItem }) {
   const [scanning, setScanning] = useState(false);
   const scannerRef = useRef(null);
+  const handleItem = item => {
+    setScanning(false);
+    setActivePage("footprint");
+    setItem(item);
+  }
 
   return (
     <div id="scanner">
       <button onClick={() => setScanning(!scanning) } style={{marginTop: 100}}>{scanning ? 'Stop' : 'Start'}</button>
       <div ref={scannerRef} style={{position: "relative"}}>
         <canvas className="drawingBuffer" style={{
-  
+          position: "absolute",
           margin: "auto",
           border: '3px solid green',
         }} width="640" height="480" />
-        {scanning ? <Camera scannerRef={scannerRef} onDetected={result => setScanning(false)} /> : null}
+        {scanning ? <Camera scannerRef={scannerRef} onDetected={item => handleItem(item)} /> : null}
       </div>
     </div>
   );
