9d55e5a7805d83ee0d6ec1fa1802d2774c615fde
yanlos <yanlos@yahoo.com>
2021-02-13 16:36:03 -0800

    ui changes to scanner
    button n spacing

--- src/Scanner.js
+++ src/Scanner.js
@@ -2,23 +2,67 @@ import { useState, useRef } from 'react';
 import './App.css';
 import Camera from './Camera';
 import './css/style.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+const buttonStyle = {
+  fontSize: "20px",
+  padding: "30px",
+  fontWeight: "bold",
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  justifyContent: 'center',
+};
+
 
 function Scanner() {
   const [scanning, setScanning] = useState(false);
   const scannerRef = useRef(null);
 
   return (
+    <main>
+
+      
     <div id="scanner">
-      <button onClick={() => setScanning(!scanning) } style={{marginTop: 100}}>{scanning ? 'Stop' : 'Start'}</button>
+
+      <div>
+        " "
+      </div>
+      <div>
+        " "
+      </div>
+      <div>
+        " "
+      </div>
+      <div>
+        " "
+      </div>
+      <div>
+        " "
+      </div>
       <div ref={scannerRef} style={{position: "relative"}}>
         <canvas className="drawingBuffer" style={{
   
           margin: "auto",
-          border: '3px solid green',
+          position: "absolute",
+          border: 'solid green',
         }} width="640" height="480" />
         {scanning ? <Camera scannerRef={scannerRef} onDetected={result => setScanning(false)} /> : null}
       </div>
+      
+      <Button 
+        onClick={() => setScanning(!scanning) }
+        variant="contained" 
+        style={{marginTop: 30}}  
+        style={buttonStyle} 
+        
+        >
+          {scanning ? 'Stop Scanning' : 'Start Scanning'}
+      </Button>
     </div>
+    </main>
   );
 }
 
