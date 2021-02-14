157f4342288debfcee1872ef6ff76a03df0c117c
Arman <armansidhu3@gmail.com>
2021-02-14 16:27:04 +0530

    Added search products feature

--- src/FindBrands.js
+++ src/FindBrands.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
@@ -9,12 +9,22 @@ import InfoIcon from '@material-ui/icons/Info';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
 
 import allbirds from './Ourbrands/allbirds.jpg';
 import yttp from './Ourbrands/YTTP.png';
 import lbp from './Ourbrands/lbp.png';
-import "typeface-montserrat"
-import "typeface-roboto"
+import veja from './Ourbrands/veja.png';
+import toms from './Ourbrands/toms.png';
+import greats from './Ourbrands/greats.png';
+import nuggets from './Ourbrands/nuggets.jpg';
+import regrained from './Ourbrands/Regrained.jpeg';
+import bobo from './Ourbrands/bobo.png';
+import barnana from './Ourbrands/barnana.jpeg'
+import organics from './Ourbrands/organics.jpg'
+import "typeface-montserrat";
+import "typeface-roboto";
+import "./App.css";
 
 
 
@@ -25,21 +35,11 @@ const tileData = [
         author: 'Sustainable Shoes From Wool',
     },
     {
-        img: yttp,
-        title: 'Youth to the People',
-        author: 'Plant-Powered Skin-Care',
-
+        img: veja,
+        title: 'VEJA',
+        author: 'Built from recycled plastic bottles, wild rubber and organic plants',
     },
-    {
-        img: lbp,
-        title: 'Love Beauty and Planet',
-        author: 'Recycled Plastic, Plant Based Ingredients',
-    },
-    {
-        img: allbirds,
-        title: 'AllBirds',
-        author: 'Sustainable Shoes From Wool',
-    },
+    
     {
         img: yttp,
         title: 'Youth to the People',
@@ -52,17 +52,64 @@ const tileData = [
         author: 'Recycled Plastic, Plant Based Ingredients',
     },
     {
-        img: allbirds,
-        title: 'AllBirds',
-        author: 'Sustainable Shoes From Wool',
+        img: barnana,
+        title: 'Barnana',
+        author: 'Chewy banana snacks'
     },
     {
-        img: yttp,
-        title: 'Youth to the People',
-        author: 'Plant-Powered Skin-Care',
-
-    },
+        img: organics,
+        title: 'Organic Basics',
+        author: 'Organic cotton, 100% eco-certified'
+    }
 ];
+
+
+const shoes = [
+    {
+        img: allbirds,
+        title: 'AllBirds',
+        author: 'Sustainable Shoes From Wool',
+    },
+    {
+        img: veja,
+        title: 'VEJA',
+        author: 'Built from recycled plastic bottles and wild rubber',
+    },
+    {
+        img: toms,
+        title: 'TOMS Earthwise',
+        author: 'Created with plant-derived materials'
+    },
+    {
+        img: greats,
+        title: 'Greats Royale Knit',
+        author: 'Made with yarn'
+    }
+]
+
+const snacks = [
+    {
+        img: nuggets,
+        title: 'Quinn Snacks',
+        author: 'Organic and GMO-free'
+    },
+    {
+        img: regrained,
+        title: 'Regrained',
+        author: 'tasty snack bars from upcycled grains'
+    },
+    {
+        img: bobo,
+        title: 'Bobo’s',
+        author: ' made with clean and simple organic ingredients'
+    },
+    {
+        img: barnana,
+        title: 'Barnana',
+        author: 'Chewy banana snacks'
+    }
+
+]
 
 
 const useStyles = makeStyles((theme) => ({
@@ -117,6 +164,121 @@ const top10Products = [
  */
 export default function TitlebarGridList() {
     const classes = useStyles();
+    const [product, setProduct] = useState('');
+
+
+    const textRef = useRef();
+
+    // const handleClick = () => {
+    //     console.log(product);
+    //     console.log(product)
+    //   };
+
+    //   const handleChange = (product) => {
+    //     setProduct(product);
+    //     console.log(product);
+    //   }
+
+
+      function myFunction() {
+        var x = document.getElementById("text-select").value;
+        document.getElementById("demo").innerHTML = "You selected: " + x;
+    }
+
+
+    function search(nameKey, myArray){
+        for (var i=0; i < myArray.length; i++) {
+            if (myArray[i].title === nameKey) {
+                return myArray[i];
+            }
+        }
+    }
+
+    function displayProduct(value) {
+
+        if ((search(value, top10Products) != null)) {
+            
+            if (value == 'Shoes') {
+                let tile = tileData[0];
+                
+            return (
+                <div>
+                    <GridList cellHeight={180} className={classes.gridList}>
+                    {/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto', background: '#357a38' }}>
+                        {/* <ListSubheader className = {classes.words} component="div">Our Featured Sustainable Products</ListSubheader> */}
+                        {/* <Typography component="h1"> Our Featured Sustainable Products </Typography> */}
+                    {/* </GridListTile> */}
+                    {shoes.map((tile) => (
+                        <GridListTile key={tile.img}>
+                            <img src={tile.img} alt={tile.title} />
+                            <GridListTileBar
+                                title={tile.title}
+                                subtitle={<span>{tile.author}</span>}
+                                actionIcon={
+                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
+                            />
+                        </GridListTile>
+                    ))}
+                </GridList>
+                </div>
+            );
+        }
+
+        else if (value == 'Skin Care') {
+            const tile = tileData[2];
+            
+        return (
+            <div>
+                <GridList cellHeight={180} className={classes.gridList}>
+                <GridListTile key={tile.img}>
+                            <img src={tile.img} alt={tile.title} />
+                            <GridListTileBar
+                                title={tile.title}
+                                subtitle={<span>{tile.author}</span>}
+                                actionIcon={
+                                    <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                        <InfoIcon />
+                                    </IconButton>
+                                }
+                            />
+                </GridListTile>
+                </GridList>
+            </div>
+        );
+    }
+
+    else if (value == 'Foods & Snacks') {
+        return (
+            <div>
+                <GridList cellHeight={180} className={classes.gridList}>
+                {/* <GridListTile key="Subheader" cols={2} style={{ height: 'auto', background: '#357a38' }}>
+                    {/* <ListSubheader className = {classes.words} component="div">Our Featured Sustainable Products</ListSubheader> */}
+                    {/* <Typography component="h1"> Our Featured Sustainable Products </Typography> */}
+                {/* </GridListTile> */}
+                {snacks.map((tile) => (
+                    <GridListTile key={tile.img}>
+                        <img src={tile.img} alt={tile.title} />
+                        <GridListTileBar
+                            title={tile.title}
+                            subtitle={<span>{tile.author}</span>}
+                            actionIcon={
+                                <IconButton aria-label={`info about ${tile.title}`} className={classes.icon}>
+                                    <InfoIcon />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                ))}
+            </GridList>
+            </div>
+        );
+    }
+    }
+    }
+
 
     return (
         <main id="main-findBrands">
@@ -159,17 +321,33 @@ export default function TitlebarGridList() {
             <br>
             </br>
             <h1 id="search-own" style={{ color: 'green', fontFamily:'"Montserrat"' }}>Or Search For Your Own</h1>
-
-
+    
     <div style={{ width: 300 }}>
-      <Autocomplete
+      <Autocomplete value={product} onChange={(event, newValue) => {
+            {setProduct(newValue)}
+          }}
         id="free-solo-demo"
         freeSolo
         options={top10Products.map((option) => option.title)}
         renderInput={(params) => (
-          <TextField {...params} label="Environmentally Friendly" margin="normal" variant="outlined" />
+          <TextField  inputRef={textRef} {...params} label="Environmentally Friendly" margin="normal" variant="outlined" 
+          />
         )}
+        
       />
+      
+      {displayProduct(product)}
+{/* {
+    <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+        //  className={classes.submit}
+            onClick={handleClick}
+        >
+            Find Product
+        </Button> } */}
 
     </div>
         </main>
